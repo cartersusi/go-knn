9b2e625b174156ef033af3cca4dc7564f996ee57
Carter Susi <carter4299@gmail.com>
2024-09-21 17:53:57 -0500

    update

--- tensor.go
+++ tensor.go
@@ -1,7 +1,10 @@
 package knn
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
+	"os"
 	"reflect"
 )
 
@@ -47,4 +50,101 @@ func (t *Tensor[T]) New(values interface{}) error {
 	t.Type = v.Type()
 
 	return nil
+}
+
+func init() {
+	gob.Register([]float32{})
+	gob.Register([][]float32{})
+	gob.Register([]float64{})
+	gob.Register([][]float64{})
+}
+
+func (t *Tensor[T]) GobEncode() ([]byte, error) {
+	var data struct {
+		Values   interface{}
+		Shape    [2]int
+		TypeName string
+		Rank     int
+	}
+
+	data.Values = t.Values
+	data.Shape = t.Shape
+	data.TypeName = t.Type.Name()
+	data.Rank = t.Rank
+
+	return gobEncode(data)
+}
+
+func (t *Tensor[T]) GobDecode(buf []byte) error {
+	var data struct {
+		Values   interface{}
+		Shape    [2]int
+		TypeName string
+		Rank     int
+	}
+
+	if err := gobDecode(buf, &data); err != nil {
+		return err
+	}
+
+	t.Values = data.Values
+	t.Shape = data.Shape
+	t.Rank = data.Rank
+
+	switch data.TypeName {
+	case "float32":
+		t.Type = reflect.TypeOf(float32(0))
+	case "float64":
+		t.Type = reflect.TypeOf(float64(0))
+	default:
+		return fmt.Errorf("unsupported type: %s", data.TypeName)
+	}
+
+	return nil
+}
+
+func SaveTensor[T float32 | float64](t *Tensor[T], filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("error creating file: %v", err)
+	}
+	defer file.Close()
+
+	encoder := gob.NewEncoder(file)
+	if err := encoder.Encode(t); err != nil {
+		return fmt.Errorf("error encoding tensor: %v", err)
+	}
+
+	return nil
+}
+
+func LoadTensor[T float32 | float64](filename string) (*Tensor[T], error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, fmt.Errorf("error opening file: %v", err)
+	}
+	defer file.Close()
+
+	var t Tensor[T]
+	decoder := gob.NewDecoder(file)
+	if err := decoder.Decode(&t); err != nil {
+		return nil, fmt.Errorf("error decoding tensor: %v", err)
+	}
+
+	return &t, nil
+}
+
+func gobEncode(data interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(data)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func gobDecode(buf []byte, data interface{}) error {
+	dec := gob.NewDecoder(bytes.NewBuffer(buf))
+	return dec.Decode(data)
 }
